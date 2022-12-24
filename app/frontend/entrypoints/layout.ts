da3cf131aec7b0148703821ae3505f4239f20d82
Truong Dung <truonghoangdung1987hp@gmail.com>
2022-12-25 04:28:15 +0700

    Fix

--- app/frontend/entrypoints/layout.ts
+++ app/frontend/entrypoints/layout.ts
@@ -3,4 +3,7 @@ import 'virtual:windi-devtools'
 import '~/styles/application.css'
 import 'flowbite';
 import 'flowbite-react';
+require("@rails/ujs").start()
 require('@rails/activestorage').start()
+require("local-time").start()
+require("@hotwired/turbo-rails")
--- lib/samurai/core/version.rb
+++ lib/samurai/core/version.rb
@@ -1,5 +1,5 @@
 module Samurai
   module Core
-    VERSION = "0.0.2-rc.28"
+    VERSION = "0.0.2-rc.29"
   end
 end
